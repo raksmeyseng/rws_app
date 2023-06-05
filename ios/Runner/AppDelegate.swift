092e9b3853f86b21fa8fa65aae63342ad357336b
Raksmey <mwg.sengraksmey@gmail.com>
2023-06-05 08:45:37 +0700

    update info list for ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    [GMSServices provideAPIKey:@"AIzaSyBjaBN8v_6ZMFM6rzs1NeUOMrXCJJInSzU"];
+    GMSServices.provideAPIKey("YAIzaSyBjaBN8v_6ZMFM6rzs1NeUOMrXCJJInSzU")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
